6c89b019069bfc1805ff45e6bf2bb8b0c4378c2b
Matthew Bates <matthew.bates@bedegaming.com>
2019-10-23 01:44:07 +0100

    [Major] Upgrade to hapi 17+

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,21 +8,28 @@ module.exports = {
   },
   "parserOptions": {
     "sourceType": "script",
+    "ecmaVersion": 2018,
     "ecmaFeatures": {
       "modules": false,
       "impliedStrict": false
     }
   },
   rules: {
+    "strict": [2, "global"],
+    "indent": ["error", 2],
+    "linebreak-style": 0,
     "max-len": [2, 120, 2, {
       ignoreUrls: true,
       ignoreComments: false
     }],
     "comma-dangle": [2, 'never'],
     "no-restricted-syntax": [0],
+    "no-multi-assign": 0,
+    "no-return-assign": 0,
     "strict": [2, "global"],
     "global-require": 0,
     "padded-blocks": 0,
-    "no-underscore-dangle": 0
+    "no-underscore-dangle": 0,
+    "no-param-reassign": 0
   }
 };
--- test/explicitScope.spec.js
+++ test/explicitScope.spec.js
@@ -1,331 +1,288 @@
 'use strict';
 
-const Hapi = require('hapi');
+const Hapi = require('@hapi/hapi');
 
-const { expect } = require('code');
-const { describe, it } = exports.lab = require('lab').script();
+const { expect } = require('@hapi/code');
 
-const setAuthSchemeWithScope = (scope) =>
-  () => ({
-    authenticate(request, reply) {
-      return reply.continue({ credentials: { scope } });
-    }
-  });
+const { describe, it } = exports.lab = require('@hapi/lab').script();
+
+const setAuthSchemeWithScope = (scope) => () => ({
+  authenticate(request, h) {
+    return h.authenticated({ credentials: { scope } });
+  }
+});
 
 describe('explicit scope', () => {
 
-  it('assigns scopeContext to auth artifacts', (done) => {
+  it('assigns scopeContext to auth artifacts', async () => {
     const server = new Hapi.Server();
-    server.connection();
 
     server.auth.scheme('scopeTest', setAuthSchemeWithScope([]));
     server.auth.strategy('scope', 'scopeTest');
 
-    server.register(require('../'), () => {
+    await server.register(require('../'));
 
-      server.route({
-        method: 'GET',
-        path: '/artifacts',
-        config: {
-          auth: 'scope',
-          plugins: {
-            inferredScope: []
-          },
-          handler: (request, reply) => reply(request.auth.artifacts).code(200)
-        }
-      });
-
-      server.inject('/artifacts', (res) => {
-        expect(res.statusCode).to.equal(200);
-        expect(res.result.scopeContext).to.exist();
-        done();
-      });
+    server.route({
+      method: 'GET',
+      path: '/artifacts',
+      options: {
+        auth: 'scope',
+        plugins: {
+          inferredScope: []
+        },
+        handler: (request) => request.auth.artifacts
+      }
     });
+
+    const res = await server.inject('/artifacts');
+    expect(res.statusCode).to.equal(200);
+    expect(res.result.scopeContext).to.exist();
   });
 
-  it('builds scopeContext object', (done) => {
+  it('builds scopeContext object', async () => {
     const server = new Hapi.Server();
-    server.connection();
 
     server.auth.scheme('scopeTest', setAuthSchemeWithScope(['scope1', 'scope2']));
     server.auth.strategy('scope', 'scopeTest');
 
-    server.register(require('../'), () => {
+    await server.register(require('../'));
 
-      server.route({
-        method: 'GET',
-        path: '/artifacts',
-        config: {
-          auth: 'scope',
-          plugins: {
-            inferredScope: []
-          },
-          handler: (request, reply) => reply(request.auth.artifacts).code(200)
-        }
-      });
-
-      server.inject('/artifacts', (res) => {
-        expect(res.statusCode).to.equal(200);
-        expect(res.result.scopeContext).to.exist();
-        expect(res.result.scopeContext.scope1).to.exist();
-        expect(res.result.scopeContext.scope2).to.exist();
-        done();
-      });
+    server.route({
+      method: 'GET',
+      path: '/artifacts',
+      config: {
+        auth: 'scope',
+        plugins: {
+          inferredScope: []
+        },
+        handler: (request, h) => h.response(request.auth.artifacts).code(200)
+      }
     });
+
+    const res = await server.inject('/artifacts');
+    expect(res.statusCode).to.equal(200);
+    expect(res.result.scopeContext).to.exist();
+    expect(res.result.scopeContext.scope1).to.exist();
+    expect(res.result.scopeContext.scope2).to.exist();
   });
 
-  it('authenticates on matching an explicit single scope', (done) => {
+  it('authenticates on matching an explicit single scope', async () => {
     const server = new Hapi.Server();
-    server.connection();
 
     server.auth.scheme('scopeTest', setAuthSchemeWithScope(['scope']));
     server.auth.strategy('scope', 'scopeTest');
 
-    server.register(require('../'), () => {
+    await server.register(require('../'));
 
-      server.route({
-        method: 'GET',
-        path: '/singlescope',
-        config: {
-          auth: 'scope',
-          plugins: {
-            inferredScope: ['scope']
-          },
-          handler: (request, reply) => reply().code(200)
-        }
-      });
-
-      server.inject('/singlescope', (res) => {
-        expect(res.statusCode).to.equal(200);
-        done();
-      });
+    server.route({
+      method: 'GET',
+      path: '/singlescope',
+      config: {
+        auth: 'scope',
+        plugins: {
+          inferredScope: ['scope']
+        },
+        handler: (request, h) => h.response().code(200)
+      }
     });
+
+    const res = await server.inject('/singlescope');
+    expect(res.statusCode).to.equal(200);
   });
 
-  it('authenticates on matching explicit multiple scopes', (done) => {
+  it('authenticates on matching explicit multiple scopes', async () => {
     const server = new Hapi.Server();
-    server.connection();
 
     server.auth.scheme('scopeTest', setAuthSchemeWithScope(['scope1', 'scope2']));
     server.auth.strategy('scope', 'scopeTest');
 
-    server.register(require('../'), () => {
+    await server.register(require('../'));
 
-      server.route({
-        method: 'GET',
-        path: '/multiplescopes',
-        config: {
-          auth: 'scope',
-          plugins: {
-            inferredScope: ['scope1', 'scope2']
-          },
-          handler: (request, reply) => reply().code(200)
-        }
-      });
-
-      server.inject('/multiplescopes', (res) => {
-        expect(res.statusCode).to.equal(200);
-        done();
-      });
+    server.route({
+      method: 'GET',
+      path: '/multiplescopes',
+      config: {
+        auth: 'scope',
+        plugins: {
+          inferredScope: ['scope1', 'scope2']
+        },
+        handler: (request, h) => h.response().code(200)
+      }
     });
+
+    const res = await server.inject('/multiplescopes');
+    expect(res.statusCode).to.equal(200);
   });
 
-  it('authenticates on matching at least one explicit scope', (done) => {
+  it('authenticates on matching at least one explicit scope', async () => {
     const server = new Hapi.Server();
-    server.connection();
 
     server.auth.scheme('scopeTest', setAuthSchemeWithScope(['scope1', 'scope2']));
     server.auth.strategy('scope', 'scopeTest');
 
-    server.register(require('../'), () => {
+    await server.register(require('../'));
 
-      server.route({
-        method: 'GET',
-        path: '/eitherscope',
-        config: {
-          auth: 'scope',
-          plugins: {
-            inferredScope: ['scope2']
-          },
-          handler: (request, reply) => reply().code(200)
-        }
-      });
-
-      server.inject('/eitherscope', (res) => {
-        expect(res.statusCode).to.equal(200);
-        done();
-      });
+    server.route({
+      method: 'GET',
+      path: '/eitherscope',
+      config: {
+        auth: 'scope',
+        plugins: {
+          inferredScope: ['scope2']
+        },
+        handler: (request, h) => h.response().code(200)
+      }
     });
+
+    const res = await server.inject('/eitherscope');
+    expect(res.statusCode).to.equal(200);
   });
 
-  it('authenticates when the route is configured with no scopes (empty array)', (done) => {
+  it('authenticates when the route is configured with no scopes (empty array)', async () => {
     const server = new Hapi.Server();
-    server.connection();
 
     server.auth.scheme('scopeTest', setAuthSchemeWithScope(['scope1', 'scope2']));
     server.auth.strategy('scope', 'scopeTest');
 
-    server.register(require('../'), () => {
+    await server.register(require('../'));
 
-      server.route({
-        method: 'GET',
-        path: '/emptyscopes',
-        config: {
-          auth: 'scope',
-          plugins: {
-            inferredScope: []
-          },
-          handler: (request, reply) => reply().code(200)
-        }
-      });
-
-      server.inject('/emptyscopes', (res) => {
-        expect(res.statusCode).to.equal(200);
-        done();
-      });
+    server.route({
+      method: 'GET',
+      path: '/emptyscopes',
+      config: {
+        auth: 'scope',
+        plugins: {
+          inferredScope: []
+        },
+        handler: (request, h) => h.response().code(200)
+      }
     });
+
+    const res = await server.inject('/emptyscopes');
+    expect(res.statusCode).to.equal(200);
   });
 
-  it('authenticates a required scope', (done) => {
+  it('authenticates a required scope', async () => {
     const server = new Hapi.Server();
-    server.connection();
 
     server.auth.scheme('scopeTest', setAuthSchemeWithScope(['scope1', 'scope2']));
     server.auth.strategy('scope', 'scopeTest');
 
-    server.register(require('../'), () => {
+    await server.register(require('../'));
 
-      server.route({
-        method: 'GET',
-        path: '/requiredscope',
-        config: {
-          auth: 'scope',
-          plugins: {
-            inferredScope: ['+scope1']
-          },
-          handler: (request, reply) => reply().code(200)
-        }
-      });
-
-      server.inject('/requiredscope', (res) => {
-        expect(res.statusCode).to.equal(200);
-        done();
-      });
+    server.route({
+      method: 'GET',
+      path: '/requiredscope',
+      config: {
+        auth: 'scope',
+        plugins: {
+          inferredScope: ['+scope1']
+        },
+        handler: (request, h) => h.response().code(200)
+      }
     });
+
+    const res = await server.inject('/requiredscope');
+    expect(res.statusCode).to.equal(200);
   });
 
-  it('fails to authenticates when a required scope is missing', (done) => {
+  it('fails to authenticates when a required scope is missing', async () => {
     const server = new Hapi.Server();
-    server.connection();
 
     server.auth.scheme('scopeTest', setAuthSchemeWithScope(['scope2']));
     server.auth.strategy('scope', 'scopeTest');
 
-    server.register(require('../'), () => {
+    await server.register(require('../'));
 
-      server.route({
-        method: 'GET',
-        path: '/requiredscope',
-        config: {
-          auth: 'scope',
-          plugins: {
-            inferredScope: ['+scope1']
-          },
-          handler: (request, reply) => reply().code(200)
-        }
-      });
-
-      server.inject('/requiredscope', (res) => {
-        expect(res.statusCode).to.equal(403);
-        done();
-      });
+    server.route({
+      method: 'GET',
+      path: '/requiredscope',
+      config: {
+        auth: 'scope',
+        plugins: {
+          inferredScope: ['+scope1']
+        },
+        handler: (request, h) => h.response().code(200)
+      }
     });
+
+    const res = await server.inject('/requiredscope');
+    expect(res.statusCode).to.equal(403);
   });
 
-  it('authenticates a missing forbidden scope', (done) => {
+  it('authenticates a missing forbidden scope', async () => {
     const server = new Hapi.Server();
-    server.connection();
 
     server.auth.scheme('scopeTest', setAuthSchemeWithScope(['scope2']));
     server.auth.strategy('scope', 'scopeTest');
 
-    server.register(require('../'), () => {
+    await server.register(require('../'));
 
-      server.route({
-        method: 'GET',
-        path: '/forbiddenscope',
-        config: {
-          auth: 'scope',
-          plugins: {
-            inferredScope: ['!scope1', 'scope2']
-          },
-          handler: (request, reply) => reply().code(200)
-        }
-      });
-
-      server.inject('/forbiddenscope', (res) => {
-        expect(res.statusCode).to.equal(200);
-        done();
-      });
+    server.route({
+      method: 'GET',
+      path: '/forbiddenscope',
+      config: {
+        auth: 'scope',
+        plugins: {
+          inferredScope: ['!scope1', 'scope2']
+        },
+        handler: (request, h) => h.response().code(200)
+      }
     });
+
+    const res = await server.inject('/forbiddenscope');
+    expect(res.statusCode).to.equal(200);
   });
 
-  it('fails to authenticates when a forbidden scope exists', (done) => {
+  it('fails to authenticates when a forbidden scope exists', async () => {
     const server = new Hapi.Server();
-    server.connection();
 
     server.auth.scheme('scopeTest', setAuthSchemeWithScope(['scope1', 'scope2']));
     server.auth.strategy('scope', 'scopeTest');
 
-    server.register(require('../'), () => {
+    await server.register(require('../'));
 
-      server.route({
-        method: 'GET',
-        path: '/forbiddenscope',
-        config: {
-          auth: 'scope',
-          plugins: {
-            inferredScope: ['!scope2']
-          },
-          handler: (request, reply) => reply().code(200)
-        }
-      });
-
-      server.inject('/forbiddenscope', (res) => {
-        expect(res.statusCode).to.equal(403);
-        done();
-      });
+    server.route({
+      method: 'GET',
+      path: '/forbiddenscope',
+      config: {
+        auth: 'scope',
+        plugins: {
+          inferredScope: ['!scope2']
+        },
+        handler: (request, h) => h.response().code(200)
+      }
     });
+
+    const res = await server.inject('/forbiddenscope');
+    expect(res.statusCode).to.equal(403);
   });
 
-  it('dynamically binds request data to scopes', (done) => {
+
+  it('dynamically binds request data to scopes', async () => {
     const server = new Hapi.Server();
-    server.connection();
 
     server.auth.scheme('scopeTest', setAuthSchemeWithScope(['scope']));
     server.auth.strategy('scope', 'scopeTest');
 
-    server.register(require('../'), () => {
+    await server.register(require('../'));
 
-      server.route({
-        method: 'GET',
-        path: '/groupedscope/{scope}',
-        config: {
-          auth: 'scope',
-          plugins: {
-            inferredScope: ['{params.scope}']
-          },
-          handler: (request, reply) => reply(request.auth.artifacts).code(200)
-        }
-      });
-
-      server.inject('/groupedscope/scope', (res) => {
-        expect(res.statusCode).to.equal(200);
-        expect(res.result.scopeContext).to.exist();
-        expect(res.result.scopeContext.scope).to.exist();
-        done();
-      });
+    server.route({
+      method: 'GET',
+      path: '/groupedscope/{scope}',
+      config: {
+        auth: 'scope',
+        plugins: {
+          inferredScope: ['{params.scope}']
+        },
+        handler: (request, h) => h.response(request.auth.artifacts).code(200)
+      }
     });
+
+    const res = await server.inject('/groupedscope/scope');
+    expect(res.statusCode).to.equal(200);
+    expect(res.result.scopeContext).to.exist();
+    expect(res.result.scopeContext.scope).to.exist();
   });
 
 });
